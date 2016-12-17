543c33e4794d76e4501dee0125015c6f67680606
20d_kon <denis.kondranin@graphics.cs.msu.ru>
2016-12-17 15:10:35 +0300

    MPI+OpenMP version

--- bluegene_test.sh
+++ bluegene_test.sh
@@ -1,16 +1,16 @@
 rm *.out
 rm *.err
-mpixlc_r jac_2d.c
-nthreads=( 1 )
+mpixlc_r -qsmp=omp jac_2d.c
+nthreads=( 2 4 8 16 32 64 128)
 kruns=10
 SUBMIT=mpisubmit.bg
-sizes=( 18 34 66 130 258 514 1026 2050 )
+sizes=( 18 34 66 130 258 514 1026 2050 4098 8198)
 for nthread in ${nthreads[@]}
 do
     for size in ${sizes[@]}
     do
         echo $size $nthread
-        $SUBMIT -n $nthread -stdout $nthread+$size.out -w 00:15:00 a.out -- $kruns $size
+        $SUBMIT -n $nthread  -e "OMP_NUM_THREADS=4" -stdout $nthread+$size.out -w 00:15:00 a.out -- $kruns $size
     done
 done
 llq -u edu-cmc-pod16-010
